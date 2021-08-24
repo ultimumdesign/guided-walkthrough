cf0e69fa97479e18015ee8432e58ce94ffec1b7c
Jan Arcalas <jan.arcalas@recro.com>
2021-08-24 07:52:36 -0600

    fix: cleanup for buttons

--- src/recro-guided-walkthrough/index.js
+++ src/recro-guided-walkthrough/index.js
@@ -17,13 +17,15 @@ const view = (state, { updateState }) => {
           <now-card-header tagline={{ label: 'Guided Walkthrough' }} />
           <h3>{data[state.selectedStep].title.value}</h3>
           <now-rich-text html={data[state.selectedStep].rich_description.value} />
-          <now-card-divider />
-          <now-card-actions items={[
-            { label: 'Back', variant: 'secondary-negative' },
-            { label: 'Next', variant: 'secondary-positive' },
-            { label: 'Hide' }
-          ]}
+          <now-card-actions
+            items={[
+              { label: 'Back' },
+              { label: 'Next' },
+              { label: 'Hide' }
+            ]}
           />
+          <now-card-divider />
+          <now-card-footer label={{ start: '', end: (state.selectedStep + 1) + 'of ' + data.length }} split='equal' />
         </now-card>
       </div>
     )
@@ -33,7 +35,7 @@ const view = (state, { updateState }) => {
         <now-button-bare
           label='Show Guided Walkthrough' variant='primary'
           size='md' iconStart='icon-arrow-up' iconEnd='' configAria={{}}
-          on-click={e => updateState({ visible: true })}
+          on-click={() => updateState({ visible: true })}
         />
       </div>
     )
@@ -42,11 +44,12 @@ const view = (state, { updateState }) => {
 
 createCustomElement('recro-guided-walkthrough', {
   actionHandlers: {
-    'NOW_CARD_ACTIONS#ACTION_CLICKED': ({ action, updateState, state }) => {
+    'NOW_CARD_ACTIONS#ACTION_CLICKED': ({ action, updateState, state, properties }) => {
+      const data = properties.data
       const buttonAction = action.payload.action.label.toUpperCase()
       console.debug(buttonAction)
-      if (buttonAction === 'BACK') updateState({ selectedStep: state.selectedStep - 1 })
-      if (buttonAction === 'NEXT') updateState({ selectedStep: state.selectedStep + 1 })
+      if (buttonAction === 'BACK' && state.selectedStep > 0) updateState({ selectedStep: state.selectedStep - 1 })
+      if (buttonAction === 'NEXT' && (state.selectedStep + 1 < data.length)) updateState({ selectedStep: state.selectedStep + 1 })
       if (buttonAction === 'HIDE') updateState({ visible: false })
     }
   },
